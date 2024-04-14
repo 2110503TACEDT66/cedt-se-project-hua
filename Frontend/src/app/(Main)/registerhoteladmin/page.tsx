f4d82e1864bf2d375e1e7669a8fda4797da48b4f
Pacharapol K. <pcr.pol101@gmail.com>
2024-04-15 02:09:40 +0700

    [US1-4] styling registerPage

--- Frontend/src/app/(Main)/registerhoteladmin/page.tsx
+++ Frontend/src/app/(Main)/registerhoteladmin/page.tsx
@@ -6,6 +6,8 @@ import registerUser from '@/libs/registerUser';
 import { useRouter } from 'next/navigation';
 import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
+import { IoMdArrowRoundBack } from "react-icons/io";
+import Link from "next/link";
 
 export default function RegHotelAdmin() {
     const [hotels, setHotels] = useState<HotelItem[]>([]);
@@ -60,7 +62,9 @@ export default function RegHotelAdmin() {
             registerUser(payload)
             .then(() => {
                 toast.success('Register Success');
-                router.push('/profile')
+                setTimeout(() => {
+                    router.push('/profile')
+                }, 1000)
             })
             .catch((err) => {
                 toast.error('Register Failed');
@@ -70,22 +74,32 @@ export default function RegHotelAdmin() {
     }
 
     return (
-        <div className="flex justify-center items-center h-scree bg-cover bg-center bg-no-repeat">
-        <form className="backdrop-blur-sm rounded-lg my-5 py-3 w-[20%] text-center h-[60%] absolute top-20 bg-[#D9D9D9]">
-            <div className="text-5xl text-center text-white font-bold">Register</div>
+        <div className="flex justify-center items-center bg-cover bg-center bg-no-repeat">
+        <form className="bg-purple-400/30 backdrop-blur-sm rounded-lg my-5 px-3 py-5 w-[50%] text-center h-[60%]">
+            <div className="text-5xl text-center text-black font-bold p-6">Register HotelAdmin</div>
             <InputCompo type="text" name="name" text="Name" handleChange={handleChange}/>
             <InputCompo type="text" name="tel" text="Telephone Number" handleChange={handleChange}/>
             <InputCompo type="email" name="email" text="Email" handleChange={handleChange}/>
             <InputCompo type="password" name="password" text="Password" handleChange={handleChange}/>
             <div className="flex justify-center items-center  my-7">
-                <select onChange={handleSelectChange}>
+                <select onChange={handleSelectChange}
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[75%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>
                     <option hidden>Select Hotel</option>
                     {hotels.map(item => (
                         <option key={item.id} value={item.id}>{item.name}</option>
                     ))}
                 </select>
             </div>
-            <button onClick={handleSubmit} type="submit" className="bg-black hover:bg-blue-700 text-white p-2 font-medium rounded-full absolute bottom-7 left-20 right-20">Register</button>
+            <div className="flex flex-col gap-4 items-center">
+                <button onClick={handleSubmit} className="bg-black hover:scale-105 transition duration-100 
+                text-white p-2 font-medium rounded-full px-5 text-xl">
+                    Register
+                </button>
+                <Link href="/" className="bg-black hover:scale-105 transition duration-100 
+                text-white p-2 font-medium rounded-full px-5 text-xl flex items-center gap-1" >
+                    <IoMdArrowRoundBack/> Back
+                </Link>
+            </div>
         </form>
         </div>
     );
