972b85cf6f2e8470162ebb911999227ae8adeb76
Taspol <taspolsd@gmail.com>
2024-04-22 13:07:21 +0700

    change RoomCard button name

--- Frontend/src/components/RoomCard.tsx
+++ Frontend/src/components/RoomCard.tsx
@@ -21,7 +21,7 @@ export default function RoomCard({RoomItem,setRoom}:{RoomItem:RoomItem,setRoom:F
                         <div className="">Price : {RoomItem.price} Baht/Day</div>
                     </div>
                     
-                    <button onClick={() => {setRoom(RoomItem._id,RoomItem.roomNo)}} className="absolute bottom-2 right-2 rounded-xl py-2 px-3 text-white font-medium bg-purple-400 transition duration-100 hover:scale-105">Booking</button>
+                    <button onClick={() => {setRoom(RoomItem._id,RoomItem.roomNo)}} className="absolute bottom-2 right-2 rounded-xl py-2 px-3 text-white font-medium bg-purple-400 transition duration-100 hover:scale-105">Select</button>
                     
                 </div>
             </div>
