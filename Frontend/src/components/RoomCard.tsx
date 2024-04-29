d6bceeeb44ed9776c276b042fc6b97c8f48c7c29
Pacharapol K. <pcr.pol101@gmail.com>
2024-04-29 15:43:44 +0700

    [fix] refactor some code - remove unused page or component - updateBookingDB fix ratingNum Payload - average rating only calculated from booking that rated

--- Frontend/src/components/RoomCard.tsx
+++ Frontend/src/components/RoomCard.tsx
@@ -1,18 +1,14 @@
-
 import Image from "next/image";
-import RoomInteract from "./RoomInteract";
-import Link from "next/link";
-
 
 export default function RoomCard({RoomItem,setRoom}:{RoomItem:RoomItem,setRoom:Function}){
     return(
-            // <RoomInteract>
             <div className="flex flex-col w-full h-64 rounded-lg shadow-lg bg-white">
                 <div className="w-full h-[70%] relative rounded-t-lg ">
                 <Image src={RoomItem.picture}
                 alt='room'
                 fill={true}
-                className="object-cover rounded-t-lg"/>
+                className="object-cover rounded-t-lg"
+                sizes="100vh"/>
                 </div>
                 <div className="flex relative p-3 rounded-lg">
                     <div className="text-medium text-black mx-5">
