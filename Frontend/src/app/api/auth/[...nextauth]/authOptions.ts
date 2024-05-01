b25c7745b1b9d4471ab366df06f5c6a540dedc62
Caesar <nhopphasit55@gmail.com>
2024-05-01 13:58:04 +0700

    feat: love ice cream

--- Frontend/src/app/api/auth/[...nextauth]/authOptions.ts
+++ Frontend/src/app/api/auth/[...nextauth]/authOptions.ts
@@ -18,17 +18,20 @@ export const authOptions: AuthOptions = {
               password: { label: "Password", type: "password" }
             },
             async authorize(credentials, req) {
-              if (!credentials) return null;
+              if (!credentials) {
+                console.error("No credential provided");
+                return null;
+              };
 
-              const user = await userLogin(credentials.email, credentials.password);
-        
+              console.log("Me got cred", credentials);
+              const user = await userLogin(credentials.email, credentials.password) as any | null;
+              console.log("Me got user", user);
               if (user) {
                 // Any object returned will be saved in `user` property of the JWT
-                return user
+                return user;
               } else {
                 // If you return null then an error will be displayed advising the user to check their details.
-                return null
-        
+                return null;
                 // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
               }
             }
--- Frontend/src/libs/userLogin.tsx
+++ Frontend/src/libs/userLogin.tsx
@@ -1,16 +1,21 @@
-export default async function userLogin(userEmail: string, userPassword: string) {
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/login`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            email: userEmail,
-            password: userPassword
-        }),
-    });
-
-    if (!response.ok) throw new Error("Failed to login");
-
-    return response.json();
+export default async function userLogin(
+  userEmail: string,
+  userPassword: string
+) {
+  console.log("Me got email", userEmail);
+  console.log("Me got password", userPassword);
+  const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: userEmail,
+      password: userPassword,
+    }),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+    console.log("Me got response", response);
+    return response;
 }
